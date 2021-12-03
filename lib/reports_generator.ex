2edbb7a524f411afee602587c62be4f7a04d2746
Luiz Henrique <7henrique18@gmail.com>
2021-12-03 01:58:33 -0300

    :sparkles: Returning the highest value

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -7,6 +7,10 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  # "report_complete.csv" |> ReportsGenerator.build() |> ReportsGenerator.fetch_higher_cost()
+
+  def fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+
   def sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
 
   defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
